bfc406dcdb352ae98fce2e51595f7ec3fd4bf47e
CheeseWHam <alexandre.walsh00@gmail.com>
2014-09-23 20:17:08 -0700

    Problem 2 Optimization 1

--- Problem2/src/problem2/Problem2.java
+++ Problem2/src/problem2/Problem2.java
@@ -1,21 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package problem2;
 
-/**
- *
- * @author Alex
- */
 public class Problem2 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int limit = 4000000;
+        int prev = 0;
+        int beforeprev = 1;
+        int sum = 0;
+        int fib = prev + beforeprev;
+
+        do {
+            if (fib % 2 == 0) {
+                sum += fib;
+            }
+            fib = prev + beforeprev;
+            beforeprev = prev;
+            prev = fib;
+        } while (fib < limit);
+        System.out.println(sum);
     }
-    
 }
